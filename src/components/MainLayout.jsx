cfde1681548f980d22fe0e59a42461862c637613
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-07-25 15:00:28 +0100

    1.22

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, Outlet} from 'react-router-dom'
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
  
 } from '@ant-design/icons';
-import {AiOutlineDashboard, AiOutlineShoppingCart, AiOutlineUser, AiOutlineBgColors} from 'react-icons/ai';
+import {AiOutlineDashboard, AiOutlineShoppingCart, AiOutlineUser, AiOutlineBgColors } from 'react-icons/ai';
+import {IoIosNotificationsOutline} from 'react-icons/io'
 import {SiBrandfolder} from 'react-icons/si'
 import {BiCategoryAlt} from 'react-icons/bi';
 import {ImBlog} from 'react-icons/im'
 import {FaClipboardList, FaBloggerB} from 'react-icons/fa';
-import { Layout, Menu, Button, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
 const { Header, Sider, Content } = Layout;
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -139,23 +140,38 @@ const MainLayout = () => {
           ]}
         />
       </Sider>
-      <Layout>
+      <Layout className='site-layout'>
         <Header
+        className='d-flex justify-content-between ps-1 pe-5'
           style={{
             padding: 0,
             background: colorBgContainer,
           }}
         >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
+       
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: 'trigger',
+              onClick: () => setCollapsed(!collapsed)
+            }
+          )}
+          <div className='d-flex gap-4 align-items-center'>
+            <div className='position-relative'>
+              < IoIosNotificationsOutline className='fs-4'/>
+              <span className='badge bg-warning rounded-circle p-1 position-absolute'>3</span>
+              </div>
+            <div className='d-flex gap-3 align-items-center'>
+              <div>
+                <img width={32} height={32}  src='https://stroyka-admin.html.themeforest.scompiler.ru/variants/ltr/images/customers/customer-4-64x64.jpg' alt='ppimage' />
+              </div>
+              <div>
+                <h5 className='mb-0'>Klaus</h5>
+                <p className='mb-0'>nickoklaus5@gmail.com</p>
+              </div>
+            </div>
+
+          </div>
         </Header>
         <Content
           style={{
@@ -165,7 +181,7 @@ const MainLayout = () => {
             background: colorBgContainer,
           }}
         >
-          Content
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import {BsArrowDownRight, BsArrowUpRight} from 'react-icons/bs'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <h3 className='mb-4'>Dashboard</h3>
+      <div className='d-flex justify-content-between align-items-center gap-3'>
+        <div className='d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3'>
+          <div>
+            <p>Total</p> <h4 className='mb-0'>$1000</h4>
+          </div>
+          <div className='d-flex flex-column align-items-end'>
+            <h6><BsArrowDownRight /> 36%</h6>
+            <p className='mb-0'>Compared to April 2023</p>
+          </div>
+
+        </div>
+        <div className='d-flex justify-content-between align-items-end  flex-grow-1 bg-white p-3 rounded-3'>
+        <div>
+            <p>Total</p> <h4 className='mb-0'>$1000</h4>
+          </div>{"  "}
+          <div className='d-flex flex-column align-items-end'>
+          <h6 className='red'> <BsArrowDownRight /> 36%</h6>
+            <p className='mb-0'>Compared to April 2023</p>
+          </div>
+
+        </div>
+        <div className='d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3'>
+           <div>
+            <p>Total</p> <h4 className='mb-0'>$1000</h4>
+          </div>{"  "}
+          <div className='d-flex flex-column align-items-end'>
+          <h6 className='green'><BsArrowDownRight /> 36%</h6>
+            <p className='mb-0'>Compared to April 2023</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
