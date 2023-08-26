8943ee7f568927f77b7d6083c09d2467ad6c50fb
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-08-26 15:36:10 +0100

    1.36

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Login from './pages/Login';
 import ResetPassword from './pages/ResetPassword';
 import ForgotPassword from './pages/ForgotPassword';
 import MainLayout from './components/MainLayout';
+import Enquiries from './pages/Enquiries';
+import BlogList from './pages/BlogList';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/admin' element={<MainLayout />} >
           <Route index element={<Dashboard />} />
+          <Route path='enquiries' element={<Enquiries />} />
+          <Route path='blog-list' element={<BlogList />} />
         </Route>
       </Routes>
     </Router>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,120 @@
 import React from 'react'
-import {BsArrowDownRight, BsArrowUpRight} from 'react-icons/bs'
+import {BsArrowDownRight, BsArrowUpRight} from 'react-icons/bs';
+import { Table } from 'antd';
+import { Column } from '@ant-design/plots';
+
+const columns = [
+  {
+    title: 'No.',
+    dataIndex: 'key',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Product',
+    dataIndex: 'product',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+  },
+];
+const data1 = [];
+for (let i = 0; i < 46; i++) {
+  data1.push({
+    key: i,
+    name: `Edward King ${i}`,
+    product: 32,
+    status: `London, Park Lane no. ${i}`,
+  });
+}
+
 const Dashboard = () => {
+  
+  const data = [
+    {
+      type: 'Jan',
+      sales: 38,
+    },
+    {
+      type: 'Feb',
+      sales: 52,
+    },
+    {
+      type: 'March',
+      sales: 61,
+    },
+    {
+      type: 'Apr',
+      sales: 145,
+    },
+    {
+      type: 'May',
+      sales: 48,
+    },
+    {
+      type: 'June',
+      sales: 38,
+    },
+    {
+      type: 'July',
+      sales: 38,
+    },
+    {
+      type: 'Aug',
+      sales: 38,
+    },
+    {
+      type: 'Sep',
+      sales: 38,
+    },
+    {
+      type: 'Oct',
+      sales: 38,
+    },
+    {
+      type: 'Nov',
+      sales: 38,
+    },
+    {
+      type: 'Dec',
+      sales: 38,
+    },
+  ];
+  const config = {
+    data,
+    xField: 'type',
+    yField: 'sales',
+    color: ({ type }) => {
+      return "#1677ff";
+    },
+    label: {
+     
+      position: 'middle',
+     
+      style: {
+        fill: '#FFFFFF',
+        opacity:1,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      type: {
+        alias: 'Month',
+      },
+      sales: {
+        alias: 'Income',
+      },
+    },
+  };
+ 
   return (
     <div>
       <h3 className='mb-4'>Dashboard</h3>
@@ -36,6 +150,24 @@ const Dashboard = () => {
 
         </div>
       </div>
+      <div className='mt-4'>
+        <h3 className='mb-4'>Income Statics</h3>
+        <div>
+          <Column {...config} />
+        </div>
+      </div>
+      <div className='mt-4'>
+        <h3 className='mb-4'>Recent Orders</h3>
+        <div>
+          <Table 
+          columns={columns}
+          dataSource={data1}
+          />
+        </div>
+      </div>
+     
+      
+      
     </div>
   )
 }
